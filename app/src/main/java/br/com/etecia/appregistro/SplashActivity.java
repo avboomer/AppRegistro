05f5759b3af64bf0d1f560b412c5572254613851
Augusto Vinicius <augusto.vinicius137@gmail.com>
2021-10-31 14:03:45 -0300

    Splash: Adicionei putExtra para avisar LoginActivity do cadastro

--- app/src/main/java/br/com/etecia/appregistro/SplashActivity.java
+++ app/src/main/java/br/com/etecia/appregistro/SplashActivity.java
@@ -17,7 +17,10 @@ public class SplashActivity extends AppCompatActivity {
         handler.postDelayed(new Runnable() {
             @Override
             public void run() {
-                startActivity(new Intent(getApplicationContext(),LoginActivity.class));
+                Intent intent = new Intent(getApplicationContext(), LoginActivity.class);
+                intent.putExtra("modocadastro", "Splash: No register. Fix this later");
+                startActivity(intent);
+                finish();
             }
         }, 5000);
     }
